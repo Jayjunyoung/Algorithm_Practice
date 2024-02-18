4a4f9f54e0ffab10dc9b1dbc66265755b7309132
Yeom MG <threebr123@naver.com>
2024-02-18 16:17:39 +0900

    Python Study
    24/02/18

---
+++ Step_10/수학은 체육과목 입니다.py
@@ -0,0 +1,8 @@
+# 15894
+
+def solution(cnt):
+    return 3 * cnt + cnt
+
+if __name__ == "__main__":
+    cnt = int(input())
+    print(solution(cnt))
---
+++ Step_11/알고리즘 수업 4.py
@@ -0,0 +1,10 @@
+x = int(input())
+
+i = 1
+sum = 0
+while(i < x):
+    sum += i
+    i += 1
+
+print(sum)
+print(2)
