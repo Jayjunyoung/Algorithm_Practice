32af2558c22ef1c440b400d5860617ac6d90a4ea
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-28 12:48:40 +0900

    다익스트라 알고리즘 백준 14938번 문제 공부

---
+++ dijkstra/question14938.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().split("\n");
+
+let [n, m, r] = input[0].split(" ").map(Number);
+let items = input[1].split(" ").map(Number);
+
+let graph = Array.from({ length: n + 1 }, () => []);
+
+for (let i = 2; i < 2 + r; i++) {
+  let [a, b, l] = input[i].split(" ").map(Number);
+  graph[a].push([b, l]);
+  graph[b].push([a, l]);
+}
+
+function dijkstra(graph, start, n) {
+  const distances = Array(n + 1).fill(Infinity);
+  distances[start] = 0;
+  const priorityQueue = [[0, start]];
+
+  while (priorityQueue.length > 0) {
+    const [currentDistance, currentNode] = priorityQueue.shift();
+
+    if (currentDistance > distances[currentNode]) continue;
+
+    for (const [adjacent, weight] of graph[currentNode]) {
+      const distance = currentDistance + weight;
+      if (distance < distances[adjacent]) {
+        distances[adjacent] = distance;
+        priorityQueue.push([distance, adjacent]);
+        priorityQueue.sort((a, b) => a[0] - b[0]);
+      }
+    }
+  }
+
+  return distances;
+}
+
+function getMaxItems(n, m, items, graph) {
+  let maxItems = 0;
+
+  for (let i = 1; i <= n; i++) {
+    const distances = dijkstra(graph, i, n);
+    let totalItems = 0;
+    for (let j = 1; j <= n; j++) {
+      if (distances[j] <= m) {
+        totalItems += items[j - 1];
+      }
+    }
+    maxItems = Math.max(maxItems, totalItems);
+  }
+
+  return maxItems;
+}
+
+const result = getMaxItems(n, m, items, graph);
+console.log(result);
